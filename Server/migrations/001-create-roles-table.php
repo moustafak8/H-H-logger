13c5a2857391e235a46c539d48b220f75aa859d4
moustafak8 <moustafaalkamel@gmail.com>
2025-11-13 23:42:47 +0200

    created-table users

--- Server/migrations/001-create-roles-table.php
+++ Server/migrations/001-create-roles-table.php
@@ -1,8 +1,8 @@
 <?php
 include("../connection/connection.php");
 
-$sql = "CREATE TABLE roles (
-  role_id int(11) NOT NULL,
+$sql = "CREATE TABLE IF NOT EXISTS roles(
+  role_id int(11) NOT NULL PRIMARY KEY,
   role_name varchar(255) NOT NULL
 )";
 
