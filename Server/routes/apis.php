bb654c9e17e57fabfc1121614f4e4c52e6f2b117
moustafak8 <moustafaalkamel@gmail.com>
2025-11-15 09:28:38 +0200

    connected signup with create api

--- Server/routes/apis.php
+++ Server/routes/apis.php
@@ -1,6 +1,5 @@
 <?php 
 $apis = [
-    '/cars'         => ['controller' => 'CarController', 'method' => 'getCarByID'],
     '/users'         => ['controller' => 'usercontroller', 'method' => 'getUsers'],
     '/users/create' => ['controller' => 'usercontroller', 'method' => 'newUser'],
     '/users/update' => ['controller' => 'usercontroller', 'method' => 'updateuser'],
