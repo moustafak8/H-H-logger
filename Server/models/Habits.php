7bc5d93f3595bdb564cc78d37b7fabf9d091da38
moustafak8 <moustafaalkamel@gmail.com>
2025-11-15 16:23:48 +0200

    added Habits class

---
+++ Server/models/Habits.php
@@ -0,0 +1,39 @@
+<?php
+include("Model.php");
+
+class User extends Model {
+    private string $name;
+    private string $category;
+    private string $unit;
+    private string $active;
+    private string $user_id;
+    
+
+    protected static string $table = "habits";
+
+    public function __construct(array $data){
+        $this->name = $data["name"];
+        $this->category = $data["category"];
+        $this->unit = $data["unit"];
+        $this->active = $data["active"];
+        $this->user_id = $data["user_id"];
+    }
+
+    public function getemail(){
+        return $this->name;
+    }
+
+    public function setemail(string $name){
+        $this->name = $name;
+    }
+    public function __toString(){
+        return  $this->name . " | " . $this->category. " | " . $this->unit;
+    }
+    
+    public function toArray(){
+        return ["name" => $this->name, "category" => $this->category, "unit" => $this->unit ,"user_id"=>$this->user_id,"active"=>$this->active];
+    }
+
+}
+
+?>
